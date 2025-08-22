6dfdd2718f179d3b1b8787baa197cf367c05602f
SpectorShade <92053721+SpectorShade@users.noreply.github.com>
2025-08-22 11:49:45 -0600

    Update numbergame.swift

--- numbergame.swift
+++ numbergame.swift
@@ -1,28 +1,42 @@
-let targetNum = Int.random(in: 1...100) 
+let objNum = Int.random(in: 1...100) 
 var intentos = 0 
 var adivinaNum: Int? = nil 
 
 print("Adivina un numero entre 1 y 100.") 
 
-while adivinaNum != targetNum 
-{ 
- print("Escribe el numero que crees que sea: ", terminator: "") 
- if let input = readLine(), let numero = Int(input) 
- { 
-  adivinaNum = numero intentos += 1 
-  if numero < targetNum 
-  { 
-   print("El numero es mayor al tuyo") 
-  } else if numero > targetNum 
+while adivinaNum != objNum 
+{
+    print("Escribe el numero que crees que sea: ", terminator: "") 
+    
+    if let entrada = readLine(), var numero = Int(entrada) 
+    {
+        if numero < 1 || numero > 100 
+        {
+            print("Introduce un numero valido (entre 1 y 100).")
+            continue
+        }
+        
+        adivinaNum = numero 
+        intentos += 1 
+        
+        if numero < objNum 
+        {
+            print("El numero es mayor al tuyo")
+        } 
+        else if numero > objNum 
+        {
+            print("El numero es menor al tuyo")
+        } 
+        else 
+        {
+            print("Correcto") 
+            print("Te tomo \(intentos) intentos.") 
+        }
+    } 
+    else 
     { 
-     print("El numero es menor al tuyo") 
-    } else 
-      { 
-       print("Correcto") 
-       print("Te tomo \(intentos) intentos.") 
-      } 
- } else 
-   { 
-    print("Introduce un numero que sea valido.") 
-   } 
+        print("Numero invalido.")
+        print("Introduce un numero que sea valido (entre 1 y 100).") 
+    }
 }
+
